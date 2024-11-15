6900f8d82d1da5e381e8f7b5490fae1e18fd97c6
ericmariano <ericmarianoftk@gmail.com>
2024-11-15 15:18:45 -0300

    teste de commit

--- lista1/ex15.py
+++ lista1/ex15.py
@@ -7,3 +7,22 @@
 # salário bruto, quanto pagou ao INSS, quanto pagou ao sindicato, o salário líquido.
 
 # calcule os descontos e o salário líquido Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+salPerHour = float(input('How much do you earn per hour? '))
+hourWorkedMon = float(input('How many hours do you work per month? '))
+
+salaryTotal = salPerHour * hourWorkedMon
+
+ir = 11/100
+inss = 8/100
+sind = 5/100
+
+payIr = salaryTotal * ir
+payInss = salaryTotal * inss
+paySind = salaryTotal * sind
+
+discountTotal = payInss + payIr + paySind
+
+salary = salaryTotal - discountTotal
+
+print(f'You receive ${salaryTotal}, pay to INSS ${payInss} and  ${paySind} for sindicate. Your liquid salary is ${salary}')
