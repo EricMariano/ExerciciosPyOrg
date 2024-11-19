944dc8ffb766c8cb2d0577c177262aa922bc0ce6
Eric Mariano <ericbfmariano@gmail.com>
2024-11-18 23:23:11 -0300

    ex9 and ex16

--- lista1/ex9.py
+++ lista1/ex9.py
@@ -1,28 +1,31 @@
 # 9.Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. (C * 9/5) + 32 = F
 # And Fahrenheit to Celsius, asking for the user what converter his want. C = 5 * ((F-32) / 9).
 
-converterPick = input('Whats converter you wants 1°(C to F), 2°(F TO C), 3°(Both): ')
+# this question has been changed to a better practice.
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
 
-if converterPick == '1°' or converterPick == '1' or converterPick == 'first' or converterPick == 'primeiro' or converterPick == 'C to F':
-    print('°Celsius to °Fahrenheit Converter!')
-    tempC = float(input('Enter a temperature in °Celsius: '))
-    tempF = (tempC * 9/5) + 32
-    print(f'{tempC}°C in degree Fahrenheit is {tempF:.1f}°F')
+def fahrenheit_to_celsius(fahrenheit):
+    return 5 * ((fahrenheit - 32) / 9)
 
-elif converterPick == '2°' or converterPick == '2' or converterPick == 'second' or converterPick == 'segundo' or  converterPick == 'F to C':
-    print('°Fahrenheit to °Celsius Converter!')
-    tempF = float(input('Enter a temperature in °Fahrenheit: '))
-    tempC = 5 *((tempF - 32) / 9)
-    print(f'{tempF}°F in degree Celsius is {tempC:.1f}°C')
-
-else:
-    print('°Celsius to °Fahrenheit Converter!')
-    tempC = float(input('Enter a temperature in °Celsius: '))
-    tempF = (tempC * 9/5) + 32
-    print(f'{tempC}°C in degree Fahrenheit is {tempF:.1f}°F \n')
+def main():
+    print("Bem-vindo ao conversor de temperaturas!")
+    print("Escolha uma opção:")
+    print("1 - Converter Celsius para Fahrenheit")
+    print("2 - Converter Fahrenheit para Celsius")
     
-    print('°Fahrenheit to °Celsius Converter!')
-    tempF = float(input('Enter a temperature in °Fahrenheit: '))
-    tempC = 5 *((tempF - 32) / 9)
-    print(f'{tempF}°F in degree Celsius is {tempC:.1f}°C')
+    option = input("Digite sua escolha (1 ou 2): ")
     
+    if option == "1":
+        celsius = float(input("Digite a temperatura em graus Celsius: "))
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        print(f"{celsius:.2f}°C é equivalente a {fahrenheit:.2f}°F.")
+    elif option == "2":
+        fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        print(f"{fahrenheit:.2f}°F é equivalente a {celsius:.2f}°C.")
+    else:
+        print("Opção inválida. Por favor, reinicie o programa e tente novamente.")
+
+
+main()
