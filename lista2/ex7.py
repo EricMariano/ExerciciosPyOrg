9545308aca1645fe744356a0edbd3db0780b2e69
EricMariano <ericbfmariano@gmail.com>
2025-01-28 13:29:32 -0300

    update: resolução de novos exercícios.

--- lista2/ex7.py
+++ lista2/ex7.py
@@ -9,3 +9,29 @@
 # O algoritmo deve mostrar na tela as notas, a média, 
 # o conceito correspondente e a mensagem “APROVADO” se
 # o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+
+def calcular_conceito(media):
+    if 9 <= media <= 10:
+        return 'A', 'Aprovado'
+    elif 7.5 <= media < 9:
+        return 'B', 'Aprovado'
+    elif 6 <= media < 7.5:
+        return 'C', 'Aprovado'
+    elif 4 <= media < 6:
+        return 'D', 'Reprovado'
+    else:
+        return 'E', 'Reprovado'
+    
+def main():
+    nota1 = float(input('Digite sua primeira nota no semestre: '))
+    nota2 = float(input('Digite sua segunda nota no semestre: '))
+    media = (nota1 + nota2) / 2
+    conceito, status = calcular_conceito(media)  
+
+    print(f'Notas: {nota1 , nota2}')  
+    print(f'Média: {media}')  
+    print(f'Conceito: {conceito}')  
+    print(f'Status: {status}')
+
+if __name__ == '__main__':
+    main() 
