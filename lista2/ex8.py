49da5acf2a8756426063932c21100f644e7c50b6
EricMariano <ericbfmariano@gmail.com>
2025-01-29 22:19:52 -0300

    update: resolução do ex8.

--- lista2/ex8.py
+++ lista2/ex8.py
@@ -6,3 +6,28 @@
 # Triângulo Equilátero: três lados iguais;
 # Triângulo Isósceles: quaisquer dois lados iguais;
 # Triângulo Escaleno: três lados diferentes;
+
+def verifica_triangulo(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    else:
+        return False
+
+def tipo_triangulo(a, b, c):
+    if a == b == c:
+        return "Equilátero"
+    elif a == b or a == c or b == c:
+        return "Isósceles"
+    else:
+        return "Escaleno"
+
+lado1 = float(input("Digite o primeiro lado do triângulo: "))
+lado2 = float(input("Digite o segundo lado do triângulo: "))
+lado3 = float(input("Digite o terceiro lado do triângulo: "))
+
+if verifica_triangulo(lado1, lado2, lado3):
+    print("Os lados formam um triângulo.")
+    tipo = tipo_triangulo(lado1, lado2, lado3)
+    print(f"O triângulo é {tipo}.")
+else:
+    print("Os lados não formam um triângulo.")
